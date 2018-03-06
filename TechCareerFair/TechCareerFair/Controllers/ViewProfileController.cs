a0580ff0ee808e8da98d4a4e489e0836c631f891
TechCareerFair <NMCCareerFair@gmail.com>
2018-03-06 10:49:17 -0500

    Updated ViewProfileController

--- TechCareerFair/TechCareerFair/Controllers/ViewProfileController.cs
+++ TechCareerFair/TechCareerFair/Controllers/ViewProfileController.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TechCareerFair.DAL;
+using TechCareerFair.Models;
 
 namespace TechCareerFair.Controllers
 {
     public class ViewProfileController : Controller
     {
-        public ActionResult BusinessViewProfile()
+        public ActionResult BusinessViewProfile(int id)
         {
+            //BusinessRepository businessRepository = new BusinessRepository();
+            //business business = new business();
+
+            //using (businessRepository)
+            //{
+            //    business = businessRepository.SelectOne(id);
+            //}
             return View();
         }
 
-        public ActionResult ApplicantViewProfile()
+        public ActionResult ApplicantViewProfile(int id)
         {
+            //ApplicantRepository applicantRepository = new ApplicantRepository();
+            //applicant applicant = new applicant();
+
+            //using (applicantRepository)
+            //{
+            //    applicant = applicantRepository.SelectOne(id);
+            //}
             return View();
         }
     }
