46d4c066a858125ef29e78238ba43aaf07e54541
Nicholas Lewin <lewin1@ad.nmc.edu>
2018-03-06 10:07:59 -0500

    Applicant Sign Up
    Applicant Sign Up.
    (Not writing to the database yet)

---
+++ TechCareerFair/TechCareerFair/Controllers/RegisterController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TechCareerFair.Models;
+
+namespace TechCareerFair.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: Register/Create
+        public ActionResult Applicant()
+        {
+            return View();
+        }
+
+        // POST: Register/Create
+        [HttpPost]
+        public ActionResult Applicant(applicant applicant, string rePass)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+
+                if (applicant.Password != rePass)
+                {
+                    ViewBag.rePassErr = "Passwords must match.";
+                    return View();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Register/Create
+        public ActionResult Business()
+        {
+            return View();
+        }
+
+        // POST: Register/Create
+        [HttpPost]
+        public ActionResult Business(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
