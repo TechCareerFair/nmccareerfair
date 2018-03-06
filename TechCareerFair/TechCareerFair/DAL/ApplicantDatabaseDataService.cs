9c3875ecd7e6cb127bb33aa9a43525ad155fd49e
NMC Tehnology Career Fair <36733896+TechCareerFair@users.noreply.github.com>
2018-03-05 21:12:40 -0500

    Add files via upload

---
+++ TechCareerFair/TechCareerFair/DAL/ApplicantDatabaseDataService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TechCareerFair.Models;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace TechCareerFair.DAL
+{
+    public class ApplicantDatabaseDataService : IApplicantDataService, IDisposable
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public List<applicant> Read()
+        {
+            List<applicant> applicants = new List<applicant>();
+
+            using (SqlConnection connection = new SqlConnection(DataSettings.CONNECTION_STRING))
+            {
+                connection.Open();
+                StringBuilder sb = new StringBuilder();
+                sb.Append("SELECT TOP (1000) [ApplicantID],[Email],[FirstName],[LastName],[Active]");
+                sb.Append("FROM [careerfair].[applicant]");
+                String sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            applicant app = new applicant();
+                            app.ApplicantID = reader.GetInt32(0);
+                            app.Email = (reader.GetString(1));
+                            app.FirstName = (reader.GetString(2));
+                            app.LastName = (reader.GetString(3));
+                            app.Active = reader.GetBoolean(4);
+
+                            applicants.Add(app);
+                        }
+                    }
+                }
+            }
+
+            return applicants;
+        }
+
+        public void Write(List<applicant> applicants)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ TechCareerFair/TechCareerFair/DAL/DataSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace TechCareerFair.DAL
+{
+    public static class DataSettings
+    {
+        public static readonly string CONNECTION_STRING = "Server=tcp:nmccareerfair.database.windows.net,1433;Initial Catalog=CareerFair;Persist Security Info=False;User ID=piechop;Password=CIT280!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; 
+    }
+}
