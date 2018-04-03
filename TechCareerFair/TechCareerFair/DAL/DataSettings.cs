f26465d23db33e9bf946d096f50fdfb24a759413
piechop <theundersigned@hotmail.com>
2018-04-03 10:18:51 -0400

    Revert "Merge branch 'Paul'"
    This reverts commit 15c806ee916127fa74055de58489d59a5f2b726b, reversing
    changes made to 58cf50fc9bfa5b92c4e211511bdb975b55bab63b.

--- TechCareerFair/TechCareerFair/DAL/DataSettings.cs
+++ TechCareerFair/TechCareerFair/DAL/DataSettings.cs
@@ -8,9 +8,6 @@ namespace TechCareerFair.DAL
 {
     public static class DataSettings
     {
-        public static readonly string CONNECTION_STRING = "Server=tcp:nmccareerfair.database.windows.net,1433;Initial Catalog=CareerFair;Persist Security Info=False;User ID=piechop;Password=CIT280!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public static readonly string BUSINESS_DIRECTORY = "~/Content/Images/Business/";
-        public static readonly string GALLERY_DIRECTORY = "~/Content/Images/Gallery/";
-        public static readonly string RESUME_DIRECTORY = "~/Content/Images/Resume/";
+        public static readonly string CONNECTION_STRING = "Server=tcp:nmccareerfair.database.windows.net,1433;Initial Catalog=CareerFair;Persist Security Info=False;User ID=piechop;Password=CIT280!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; 
     }
 }
