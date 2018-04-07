62d1dfa2a017a943ae736a6354438abb04c553fb
piechop <theundersigned@hotmail.com>
2018-04-07 00:13:40 -0400

    Updated connection strings in Web.config

--- TechCareerFair/TechCareerFair/Models/IdentityModels.cs
+++ TechCareerFair/TechCareerFair/Models/IdentityModels.cs
@@ -9,8 +9,6 @@ namespace TechCareerFair.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string HomeTown { get; set; }
-        public System.DateTime? BirthDate { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -23,7 +21,7 @@ namespace TechCareerFair.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("CareerFair", throwIfV1Schema: false)
         {
         }
 
