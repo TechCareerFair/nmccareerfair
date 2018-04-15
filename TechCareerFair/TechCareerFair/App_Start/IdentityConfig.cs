73bd82b26bcddfebccb1028b2af9e91a971ec611
g1robo <robinsg2@ferris.edu>
2018-04-15 13:31:19 -0400

    Updated identity config
    adding ASPMS files

--- TechCareerFair/TechCareerFair/App_Start/IdentityConfig.cs
+++ TechCareerFair/TechCareerFair/App_Start/IdentityConfig.cs
@@ -27,7 +27,14 @@ namespace TechCareerFair
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Plug in your SMS service here to send a text message.
+            var soapSms = new ASPSMSX2.ASPSMSX2SoapClient("ASPSMSX2Soap");
+            soapSms.SendSimpleTextSMS(
+                System.Configuration.ConfigurationManager.AppSettings["ASPSMSUSERKEY"],
+                System.Configuration.ConfigurationManager.AppSettings["ASPSMSPASSWORD"],
+                message.Destination,
+                System.Configuration.ConfigurationManager.AppSettings["ASPSMSORIGINATOR"],
+                message.Body);
+            soapSms.Close();
             return Task.FromResult(0);
         }
     }
